627ec3e5af909de403abcf5d6db7c94e366fbf5c
p2200062 <mateo.rodriguez@etu.univ-lyon1.fr>
2024-03-22 16:13:14 +0100

    get /api/products/:productId

--- backend/routes/product.ts
+++ backend/routes/product.ts
@@ -21,7 +21,14 @@ router.get('/api/products', async (req, res, next) => {
 })
 
 router.get('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+  try {
+    const products = await Product.findAll();
+    const product = products.find(product => product.id === req.params.productId);
+    res.status(200).json(product);
+  } catch (error) {
+      console.error('Error fetching products:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
 })
 
 // You can use the authMiddleware with req.user.id to authenticate your endpoint ;)
