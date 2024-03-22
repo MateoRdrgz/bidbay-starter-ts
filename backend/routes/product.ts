16cd4a73d7969250eb65f60c9a450cf6b107e9a2
nathan <nathan.garcia@etu.univ-lyon1.fr>
2024-03-22 17:42:39 +0100

    post product

--- backend/routes/product.ts
+++ backend/routes/product.ts
@@ -8,11 +8,11 @@ const router = express.Router()
 
 router.get('/api/products', async (req, res, next) => {
   try {
-    const products = await Product.findAll({ include: [User,Bid] });
+    const products = await Product.findAll({ include: [User, Bid] });
     res.status(200).json(products);
   } catch (error) {
-      console.error('Error fetching products:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error fetching products:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 })
 
@@ -22,15 +22,23 @@ router.get('/api/products/:productId', async (req, res) => {
     const product = products.find(product => product.id === req.params.productId);
     res.status(200).json(product);
   } catch (error) {
-      console.error('Error fetching products:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error fetching products:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 })
 
 // You can use the authMiddleware with req.user.id to authenticate your endpoint ;)
 
-router.post('/api/products', (req, res) => {
-  res.status(600).send()
+router.post('/api/products',authMiddleware, async (req, res) => {
+  
+    try {
+      const newProduct: Product = req.body;
+      res.status(201).json(newProduct);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  
 })
 
 router.put('/api/products/:productId', async (req, res) => {
